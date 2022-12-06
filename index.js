a924efe313da11304f4191e56e81b0352c754e36
d.enchev <snify@abv.bg>
2022-12-06 19:05:46 +0200

    Test

--- index.js
+++ index.js
@@ -5,38 +5,39 @@ let puppeteer;
 
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
   chrome = require("chrome-aws-lambda");
-  // puppeteer = require("puppeteer-core");
-  puppeteer = require("puppeteer-extra");
+  puppeteer = require("puppeteer-core");
+
+  //puppeteer = require("puppeteer-extra");
   // const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
   // puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
 
   // Add the Imports before StealthPlugin
-  require("puppeteer-extra-plugin-stealth/evasions/chrome.app");
-  require("puppeteer-extra-plugin-stealth/evasions/chrome.csi");
-  require("puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes");
-  require("puppeteer-extra-plugin-stealth/evasions/chrome.runtime");
-  require("puppeteer-extra-plugin-stealth/evasions/defaultArgs"); // pkg warned me this one was missing
-  require("puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow");
-  require("puppeteer-extra-plugin-stealth/evasions/media.codecs");
-  require("puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency");
-  require("puppeteer-extra-plugin-stealth/evasions/navigator.languages");
-  require("puppeteer-extra-plugin-stealth/evasions/navigator.permissions");
-  require("puppeteer-extra-plugin-stealth/evasions/navigator.plugins");
-  require("puppeteer-extra-plugin-stealth/evasions/navigator.vendor");
-  require("puppeteer-extra-plugin-stealth/evasions/navigator.webdriver");
-  require("puppeteer-extra-plugin-stealth/evasions/sourceurl");
-  require("puppeteer-extra-plugin-stealth/evasions/user-agent-override");
-  require("puppeteer-extra-plugin-stealth/evasions/webgl.vendor");
-  require("puppeteer-extra-plugin-stealth/evasions/window.outerdimensions");
-  const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+  // require("puppeteer-extra-plugin-stealth/evasions/chrome.app");
+  // require("puppeteer-extra-plugin-stealth/evasions/chrome.csi");
+  // require("puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes");
+  // require("puppeteer-extra-plugin-stealth/evasions/chrome.runtime");
+  // require("puppeteer-extra-plugin-stealth/evasions/defaultArgs"); // pkg warned me this one was missing
+  // require("puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow");
+  // require("puppeteer-extra-plugin-stealth/evasions/media.codecs");
+  // require("puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency");
+  // require("puppeteer-extra-plugin-stealth/evasions/navigator.languages");
+  // require("puppeteer-extra-plugin-stealth/evasions/navigator.permissions");
+  // require("puppeteer-extra-plugin-stealth/evasions/navigator.plugins");
+  // require("puppeteer-extra-plugin-stealth/evasions/navigator.vendor");
+  // require("puppeteer-extra-plugin-stealth/evasions/navigator.webdriver");
+  // require("puppeteer-extra-plugin-stealth/evasions/sourceurl");
+  // require("puppeteer-extra-plugin-stealth/evasions/user-agent-override");
+  // require("puppeteer-extra-plugin-stealth/evasions/webgl.vendor");
+  // require("puppeteer-extra-plugin-stealth/evasions/window.outerdimensions");
+  // const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
-  puppeteer.use(StealthPlugin());
+  // puppeteer.use(StealthPlugin());
 
-  const userPrefs = require("puppeteer-extra-plugin-user-preferences");
-  puppeteer.use(userPrefs());
+  // const userPrefs = require("puppeteer-extra-plugin-user-preferences");
+  // puppeteer.use(userPrefs());
 
-  const dir = require("puppeteer-extra-plugin-user-data-dir");
-  puppeteer.use(dir());
+  // const dir = require("puppeteer-extra-plugin-user-data-dir");
+  // puppeteer.use(dir());
 } else {
   puppeteer = require("puppeteer");
 } //
