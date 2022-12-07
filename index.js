0b89cc7935c158f6ad3d81d83abe79d1a97ccce1
d.enchev <snify@abv.bg>
2022-12-07 02:01:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -47,61 +47,81 @@ app.get("/api", async (req, res) => {
   let options = {};
   let url = req.query.url;
 
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    options = {
-      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
-      headless: true,
-      ignoreHTTPSErrors: true,
-    };
-  }
+  const isUsingPuppeteer = false;
+
+  const a = performance.now();
+  let data = "";
 
   try {
-    let browser = await puppeteer.launch(options);
+    if (isUsingPuppeteer) {
+      if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        options = {
+          args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+          defaultViewport: chrome.defaultViewport,
+          executablePath: await chrome.executablePath,
+          headless: true,
+          ignoreHTTPSErrors: true,
+        };
+      }
 
-    let page = await browser.newPage();
+      let browser = await puppeteer.launch(options);
 
-    const blockedResources = [
-      // Assets
-      "*/favicon.ico",
-      ".css",
-      ".jpg",
-      ".jpeg",
-      ".gif",
-      ".png",
-      ".svg",
-      ".woff",
+      let page = await browser.newPage();
 
-      // Analytics and other fluff
-      "*.optimizely.com",
-      "everesttech.net",
-      "userzoom.com",
-      "doubleclick.net",
-      "googleadservices.com",
-      "adservice.google.com/*",
-      "connect.facebook.com",
-      "connect.facebook.net",
-      "sp.analytics.yahoo.com",
-    ];
+      const blockedResources = [
+        // Assets
+        "*/favicon.ico",
+        ".css",
+        ".jpg",
+        ".jpeg",
+        ".gif",
+        ".png",
+        ".svg",
+        ".woff",
 
-    await page._client.send("Network.setBlockedURLs", {
-      urls: blockedResources,
-    });
+        // Analytics and other fluff
+        "*.optimizely.com",
+        "everesttech.net",
+        "userzoom.com",
+        "doubleclick.net",
+        "googleadservices.com",
+        "adservice.google.com/*",
+        "connect.facebook.com",
+        "connect.facebook.net",
+        "sp.analytics.yahoo.com",
+      ];
 
-    await page.setDefaultNavigationTimeout(10000);
-    const a = performance.now();
+      await page._client.send("Network.setBlockedURLs", {
+        urls: blockedResources,
+      });
 
-    // "http://google.com"
-    // "https://www.npmjs.com/package/puppeteer-extra-plugin-stealth",
-    // "https://www.youtube.com/watch?v=4SjDI2kRzv4",
-    //"https://www.amazon.com/Kingston-240GB-Solid-SA400S37-240G/dp/B01N5IB20Q/?_encoding=UTF8&pd_rd_w=LV88W&content-id=amzn1.sym.af3f3930-6cf5-4728-bec1-8977c33a811a&pf_rd_p=af3f3930-6cf5-4728-bec1-8977c33a811a&pf_rd_r=9E49XR9DN4RHWGXTZYNW&pd_rd_wg=2dy1n&pd_rd_r=d27003b6-c8e1-42ba-bf0e-db02ac7e6a0c&ref_=pd_gw_exports_top_sellers_unrec"
+      await page.setDefaultNavigationTimeout(10000);
 
-    await page.goto(url, { waitUntil: "networkidle2" });
+      // "http://google.com"
+      // "https://www.npmjs.com/package/puppeteer-extra-plugin-stealth",
+      // "https://www.youtube.com/watch?v=4SjDI2kRzv4",
+      //"https://www.amazon.com/Kingston-240GB-Solid-SA400S37-240G/dp/B01N5IB20Q/?_encoding=UTF8&pd_rd_w=LV88W&content-id=amzn1.sym.af3f3930-6cf5-4728-bec1-8977c33a811a&pf_rd_p=af3f3930-6cf5-4728-bec1-8977c33a811a&pf_rd_r=9E49XR9DN4RHWGXTZYNW&pd_rd_wg=2dy1n&pd_rd_r=d27003b6-c8e1-42ba-bf0e-db02ac7e6a0c&ref_=pd_gw_exports_top_sellers_unrec"
+
+      await page.goto(url, { waitUntil: "networkidle2" });
+      data = await page.content();
+    } else {
+      // const proxyUrl =
+      //   "http://brd-customer-hl_32d5cbee-zone-zone1_shared_payperusage:1qlcoayay2kv@zproxy.lum-superproxy.io:22225";
+
+      const axiosResponse = await axios.get(url, {
+        headers: {
+          "user-agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36",
+        },
+        // proxy: false,
+        // httpsAgent: new HttpsProxyAgent(proxyUrl),
+      });
+
+      data = axiosResponse?.data;
+    }
 
     const b = performance.now();
-
-    res.json({ time: b - a + " ms.", data: await page.content() });
+    res.json({ time: b - a + " ms.", data });
   } catch (err) {
     console.error(err);
     return null;
