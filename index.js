b9fc0ff7bf8e207b1cf58bca2b6ae7ed1a8b36ef
d.enchev <snify@abv.bg>
2022-12-08 17:07:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.post("/api", async (req, res) => {
 
       const axiosResponse = await axios.get(url, {
         headers: {
-          "accept-encoding": "gzip, compress, deflate",
+          "accept-encoding": "gzip, compress, deflate", // TODO: add brotli support
           "user-agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36",
         },
